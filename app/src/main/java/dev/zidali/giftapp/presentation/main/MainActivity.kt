fb4cf3697438367e3e9cc2f75e6a11fbadffe784
Zida Li <zidali8@gmail.com>
2021-11-21 13:54:04 -0500

    Feature Added
    Added to and animated FAB menu:
    -Add contact
    -Add Gift
    -Add Event

--- app/src/main/java/dev/zidali/giftapp/presentation/main/MainActivity.kt
+++ app/src/main/java/dev/zidali/giftapp/presentation/main/MainActivity.kt
@@ -2,14 +2,14 @@ package dev.zidali.giftapp.presentation.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.Menu
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.TextView
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -28,6 +28,14 @@ import dev.zidali.giftapp.util.processQueue
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
 
+    //Animations
+    private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim) }
+    private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim) }
+    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim) }
+    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim) }
+    private var clicked = false
+
+    //Navigation & DrawerLayout
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navigationView: NavigationView
@@ -44,9 +52,11 @@ class MainActivity : BaseActivity() {
 
         binding.appBarMain.fabMenu.setOnClickListener {
 
-            val dialog = CreateContactFragment()
+            onMenuButtonClicked()
 
-            dialog.show(supportFragmentManager, "createContactDialog")
+//            val dialog = CreateContactFragment()
+//
+//            dialog.show(supportFragmentManager, "createContactDialog")
         }
     }
 
@@ -112,5 +122,53 @@ class MainActivity : BaseActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    /**
+     * FAB Menu Functions & Animations
+     */
+    private fun onMenuButtonClicked() {
+        setVisibility(clicked)
+        setAnimation(clicked)
+        setClickable(clicked)
+        clicked = !clicked
+    }
+
+    private fun setVisibility(clicked: Boolean) {
+        if(!clicked) {
+            binding.appBarMain.fabAddContact.visibility = View.VISIBLE
+            binding.appBarMain.fabAddGift.visibility = View.VISIBLE
+            binding.appBarMain.fabAddEvent.visibility = View.VISIBLE
+        } else {
+            binding.appBarMain.fabAddContact.visibility = View.INVISIBLE
+            binding.appBarMain.fabAddGift.visibility = View.INVISIBLE
+            binding.appBarMain.fabAddEvent.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun setAnimation(clicked:Boolean) {
+        if(!clicked) {
+            binding.appBarMain.fabAddContact.startAnimation(fromBottom)
+            binding.appBarMain.fabAddGift.startAnimation(fromBottom)
+            binding.appBarMain.fabAddEvent.startAnimation(fromBottom)
+            binding.appBarMain.fabMenu.startAnimation(rotateOpen)
+        } else {
+            binding.appBarMain.fabAddContact.startAnimation(toBottom)
+            binding.appBarMain.fabAddGift.startAnimation(toBottom)
+            binding.appBarMain.fabAddEvent.startAnimation(toBottom)
+            binding.appBarMain.fabMenu.startAnimation(rotateClose)
+        }
+    }
+
+    private fun setClickable(clicked: Boolean) {
+        if(!clicked) {
+            binding.appBarMain.fabAddContact.isClickable = true
+            binding.appBarMain.fabAddGift.isClickable = true
+            binding.appBarMain.fabAddEvent.isClickable = true
+        } else {
+            binding.appBarMain.fabAddContact.isClickable = false
+            binding.appBarMain.fabAddGift.isClickable = false
+            binding.appBarMain.fabAddEvent.isClickable = false
+        }
     }
 }
